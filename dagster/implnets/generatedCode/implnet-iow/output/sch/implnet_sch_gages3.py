7bcccd621cad2d2d4000b432d255506906222a29
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2024-08-27 12:17:04 -0800

    Spread out crawl days

--- dagster/implnets/generatedCode/implnet-iow/output/sch/implnet_sch_gages3.py
+++ dagster/implnets/generatedCode/implnet-iow/output/sch/implnet_sch_gages3.py
@@ -2,7 +2,7 @@ from dagster import schedule, DefaultScheduleStatus
 
 from jobs.implnet_jobs_gages3 import implnet_job_gages3
 
-@schedule(cron_schedule="0 0 1 * *", job=implnet_job_gages3, default_status=DefaultScheduleStatus.RUNNING, execution_timezone="US/Central")
+@schedule(cron_schedule="0 8 4 * *", job=implnet_job_gages3, default_status=DefaultScheduleStatus.RUNNING, execution_timezone="US/Central")
 def implnet_sch_gages3(_context):
     run_config = {}
     return run_config
