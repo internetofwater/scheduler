edb2599c1800723eecfd728ba36aa2b6ce97801f
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2024-09-16 15:46:32 -0400

    commented out everything but minimal example. still failing

--- code/main.py
+++ code/main.py
@@ -61,232 +61,232 @@ def gleaner(context: OpExecutionContext, source: str):
     get_dagster_logger().info(f"Gleaner returned value: '{returned_value}'")
 
 
-@op(ins={'source': In(dagster_type=dict)})
-def naburelease(context: OpExecutionContext, source: str):
-    returned_value = run_gleaner(context, "release", "SOURCEVAL")
-    r = str("returned value:{}".format(returned_value))
-    get_dagster_logger().info(f"nabu release returned  {r} ")
+# @op(ins={'source': In(dagster_type=dict)})
+# def naburelease(context: OpExecutionContext, source: str):
+#     returned_value = run_gleaner(context, "release", "SOURCEVAL")
+#     r = str("returned value:{}".format(returned_value))
+#     get_dagster_logger().info(f"nabu release returned  {r} ")
 
 
-@op(ins={'source': In(dagster_type=dict)})
-def uploadrelease(context: OpExecutionContext, source: str):
-    returned_value = run_gleaner(context, "object", "SOURCEVAL")
-    r = str("returned value:{}".format(returned_value))
-    get_dagster_logger().info(f"nabu object call release returned  {r} ")
+# @op(ins={'source': In(dagster_type=dict)})
+# def uploadrelease(context: OpExecutionContext, source: str):
+#     returned_value = run_gleaner(context, "object", "SOURCEVAL")
+#     r = str("returned value:{}".format(returned_value))
+#     get_dagster_logger().info(f"nabu object call release returned  {r} ")
 
 
-@op(ins={'source': In(dagster_type=dict)})
-def nabu_prune(context: OpExecutionContext, source: str):
-    returned_value = run_gleaner(context, "prune", "SOURCEVAL")
-    r = str("returned value:{}".format(returned_value))
-    get_dagster_logger().info(f"nabu prune returned  {r} ")
+# @op(ins={'source': In(dagster_type=dict)})
+# def nabu_prune(context: OpExecutionContext, source: str):
+#     returned_value = run_gleaner(context, "prune", "SOURCEVAL")
+#     r = str("returned value:{}".format(returned_value))
+#     get_dagster_logger().info(f"nabu prune returned  {r} ")
 
-@op(ins={'source': In(dagster_type=dict)})
-def nabu_provrelease(context, source: str):
-    returned_value = run_gleaner(context, "prov-release", "SOURCEVAL")
-    r = str("returned value:{}".format(returned_value))
-    get_dagster_logger().info(f"nabu prov-release returned  {r} ")
-
-
-@op(ins={'source': In(dagster_type=dict)})
-def nabu_provclear(context, source: str):
-    returned_value = run_gleaner(context, "prov-clear", "SOURCEVAL")
-    r = str("returned value:{}".format(returned_value))
-    get_dagster_logger().info(f"nabu prov-clear returned  {r} ")
+# @op(ins={'source': In(dagster_type=dict)})
+# def nabu_provrelease(context, source: str):
+#     returned_value = run_gleaner(context, "prov-release", "SOURCEVAL")
+#     r = str("returned value:{}".format(returned_value))
+#     get_dagster_logger().info(f"nabu prov-release returned  {r} ")
 
 
-@op(ins={"start": In(Nothing)})
-def nabu_provobject(context, source: str):
-    returned_value = run_gleaner(context, "prov-object", "SOURCEVAL")
-    r = str("returned value:{}".format(returned_value))
-    get_dagster_logger().info(f"nabu prov-object returned  {r} ")
+# @op(ins={'source': In(dagster_type=dict)})
+# def nabu_provclear(context, source: str):
+#     returned_value = run_gleaner(context, "prov-clear", "SOURCEVAL")
+#     r = str("returned value:{}".format(returned_value))
+#     get_dagster_logger().info(f"nabu prov-clear returned  {r} ")
 
 
-@op(ins={'source': In(dagster_type=dict)})
-def nabu_provdrain(context: OpExecutionContext, source: str):
-    returned_value = run_gleaner(context, "prov-drain", "SOURCEVAL")
-    r = str("returned value:{}".format(returned_value))
-    get_dagster_logger().info(f"nabu prov-drain returned  {r} ")
+# @op(ins={"start": In(Nothing)})
+# def nabu_provobject(context, source: str):
+#     returned_value = run_gleaner(context, "prov-object", "SOURCEVAL")
+#     r = str("returned value:{}".format(returned_value))
+#     get_dagster_logger().info(f"nabu prov-object returned  {r} ")
 
 
-@op(ins={'source': In(dagster_type=dict)})
-def nabu_orgsrelease(context: OpExecutionContext, source: str):
-    returned_value = run_gleaner(context, "orgs-release", "SOURCEVAL")
-    r = str("returned value:{}".format(returned_value))
-    get_dagster_logger().info(f"nabu orgs-release returned  {r} ")
+# @op(ins={'source': In(dagster_type=dict)})
+# def nabu_provdrain(context: OpExecutionContext, source: str):
+#     returned_value = run_gleaner(context, "prov-drain", "SOURCEVAL")
+#     r = str("returned value:{}".format(returned_value))
+#     get_dagster_logger().info(f"nabu prov-drain returned  {r} ")
 
 
-@op(ins={'source': In(dagster_type=dict)})
-def nabuorgs(context: OpExecutionContext, source: str):
-    returned_value = run_gleaner(context, ("orgs"), "SOURCEVAL")
-    r = str("returned value:{}".format(returned_value))
-    get_dagster_logger().info(f"nabu org load returned  {r} ")
+# @op(ins={'source': In(dagster_type=dict)})
+# def nabu_orgsrelease(context: OpExecutionContext, source: str):
+#     returned_value = run_gleaner(context, "orgs-release", "SOURCEVAL")
+#     r = str("returned value:{}".format(returned_value))
+#     get_dagster_logger().info(f"nabu orgs-release returned  {r} ")
 
 
-@op(ins={'source': In(dagster_type=dict)})
-def missingreport_s3(context: OpExecutionContext, source: str):
-    source = getSitemapSourcesFromGleaner(GLEANER_CONFIG_PATH, sourcename="SOURCEVAL")
-    source_url = source.get("url")
-    s3Minio = s3.MinioDatastore(
-        _pythonMinioAddress(GLEANER_MINIO_ADDRESS, GLEANER_MINIO_PORT), MINIO_OPTIONS
-    )
-    bucket = GLEANER_MINIO_BUCKET
-    source_name = "SOURCEVAL"
-    graphendpoint = None
-    milled = False
-    summon = True
-    returned_value = missingReport(
-        source_url,
-        bucket,
-        source_name,
-        s3Minio,
-        graphendpoint,
-        milled=milled,
-        summon=summon,
-    )
-    r = str("missing repoort returned value:{}".format(returned_value))
-    report = json.dumps(returned_value, indent=2)
-    s3Minio.putReportFile(bucket, source_name, "missing_report_s3.json", report)
-    get_dagster_logger().info(f"missing s3 report  returned  {r} ")
+# @op(ins={'source': In(dagster_type=dict)})
+# def nabuorgs(context: OpExecutionContext, source: str):
+#     returned_value = run_gleaner(context, ("orgs"), "SOURCEVAL")
+#     r = str("returned value:{}".format(returned_value))
+#     get_dagster_logger().info(f"nabu org load returned  {r} ")
 
 
-@op(ins={'source': In(dagster_type=dict)})
-def missingreport_graph(context: OpExecutionContext, source: str):
-    source = getSitemapSourcesFromGleaner(GLEANER_CONFIG_PATH, sourcename="SOURCEVAL")
-    source_url = source.get("url")
-    s3Minio = s3.MinioDatastore(
-        _pythonMinioAddress(GLEANER_MINIO_ADDRESS, GLEANER_MINIO_PORT), MINIO_OPTIONS
-    )
-    bucket = GLEANER_MINIO_BUCKET
-    source_name = "SOURCEVAL"
-
-    graphendpoint = _graphEndpoint()
-
-    milled = True
-    summon = False  # summon only off
-    returned_value = missingReport(
-        source_url,
-        bucket,
-        source_name,
-        s3Minio,
-        graphendpoint,
-        milled=milled,
-        summon=summon,
-    )
-    r = str("missing report graph returned value:{}".format(returned_value))
-    report = json.dumps(returned_value, indent=2)
-
-    s3Minio.putReportFile(bucket, source_name, "missing_report_graph.json", report)
-    get_dagster_logger().info(f"missing graph  report  returned  {r} ")
+# @op(ins={'source': In(dagster_type=dict)})
+# def missingreport_s3(context: OpExecutionContext, source: str):
+#     source = getSitemapSourcesFromGleaner(GLEANER_CONFIG_PATH, sourcename="SOURCEVAL")
+#     source_url = source.get("url")
+#     s3Minio = s3.MinioDatastore(
+#         _pythonMinioAddress(GLEANER_MINIO_ADDRESS, GLEANER_MINIO_PORT), MINIO_OPTIONS
+#     )
+#     bucket = GLEANER_MINIO_BUCKET
+#     source_name = "SOURCEVAL"
+#     graphendpoint = None
+#     milled = False
+#     summon = True
+#     returned_value = missingReport(
+#         source_url,
+#         bucket,
+#         source_name,
+#         s3Minio,
+#         graphendpoint,
+#         milled=milled,
+#         summon=summon,
+#     )
+#     r = str("missing repoort returned value:{}".format(returned_value))
+#     report = json.dumps(returned_value, indent=2)
+#     s3Minio.putReportFile(bucket, source_name, "missing_report_s3.json", report)
+#     get_dagster_logger().info(f"missing s3 report  returned  {r} ")
 
 
-@op(ins={'source': In(dagster_type=dict)})
-def graph_reports(context: OpExecutionContext, source: str):
-    source = getSitemapSourcesFromGleaner(GLEANER_CONFIG_PATH, sourcename="SOURCEVAL")
-    s3Minio = s3.MinioDatastore(
-        _pythonMinioAddress(GLEANER_MINIO_ADDRESS, GLEANER_MINIO_PORT), MINIO_OPTIONS
-    )
-    bucket = GLEANER_MINIO_BUCKET
-    source_name = "SOURCEVAL"
+# @op(ins={'source': In(dagster_type=dict)})
+# def missingreport_graph(context: OpExecutionContext, source: str):
+#     source = getSitemapSourcesFromGleaner(GLEANER_CONFIG_PATH, sourcename="SOURCEVAL")
+#     source_url = source.get("url")
+#     s3Minio = s3.MinioDatastore(
+#         _pythonMinioAddress(GLEANER_MINIO_ADDRESS, GLEANER_MINIO_PORT), MINIO_OPTIONS
+#     )
+#     bucket = GLEANER_MINIO_BUCKET
+#     source_name = "SOURCEVAL"
 
-    graphendpoint = _graphEndpoint()
+#     graphendpoint = _graphEndpoint()
 
-    returned_value = generateGraphReportsRepo(
-        source_name, graphendpoint, reportList=reportTypes["repo_detailed"]
-    )
-    r = str("returned value:{}".format(returned_value))
-    # report = json.dumps(returned_value, indent=2) # value already json.dumps
-    report = returned_value
-    s3Minio.putReportFile(bucket, source_name, "graph_stats.json", report)
-    get_dagster_logger().info(f"graph report  returned  {r} ")
+#     milled = True
+#     summon = False  # summon only off
+#     returned_value = missingReport(
+#         source_url,
+#         bucket,
+#         source_name,
+#         s3Minio,
+#         graphendpoint,
+#         milled=milled,
+#         summon=summon,
+#     )
+#     r = str("missing report graph returned value:{}".format(returned_value))
+#     report = json.dumps(returned_value, indent=2)
 
-@op(ins={'source': In(dagster_type=dict)})
-def identifier_stats(context: OpExecutionContext, source: str):
-    source = getSitemapSourcesFromGleaner(GLEANER_CONFIG_PATH, sourcename="SOURCEVAL")
-    s3Minio = s3.MinioDatastore(
-        _pythonMinioAddress(GLEANER_MINIO_ADDRESS, GLEANER_MINIO_PORT), MINIO_OPTIONS
-    )
-    bucket = GLEANER_MINIO_BUCKET
-    source_name = "SOURCEVAL"
-
-    returned_value = generateIdentifierRepo(source_name, bucket, s3Minio)
-    r = str("returned value:{}".format(returned_value))
-    # r = str('identifier stats returned value:{}'.format(returned_value))
-    report = returned_value.to_json()
-    s3Minio.putReportFile(bucket, source_name, "identifier_stats.json", report)
-    get_dagster_logger().info(f"identifer stats report  returned  {r} ")
+#     s3Minio.putReportFile(bucket, source_name, "missing_report_graph.json", report)
+#     get_dagster_logger().info(f"missing graph  report  returned  {r} ")
 
 
-@op(ins={'source': In(dagster_type=dict)})
-def bucket_urls(context: OpExecutionContext, source: dict):
-    s3Minio = s3.MinioDatastore(
-        f"{GLEANER_MINIO_ADDRESS}:{GLEANER_MINIO_PORT}", MINIO_OPTIONS
-    )
-    bucket = GLEANER_MINIO_BUCKET
-    source_name = "SOURCEVAL"
+# @op(ins={'source': In(dagster_type=dict)})
+# def graph_reports(context: OpExecutionContext, source: str):
+#     source = getSitemapSourcesFromGleaner(GLEANER_CONFIG_PATH, sourcename="SOURCEVAL")
+#     s3Minio = s3.MinioDatastore(
+#         _pythonMinioAddress(GLEANER_MINIO_ADDRESS, GLEANER_MINIO_PORT), MINIO_OPTIONS
+#     )
+#     bucket = GLEANER_MINIO_BUCKET
+#     source_name = "SOURCEVAL"
 
-    res = s3Minio.listSummonedUrls(bucket, source_name)
-    bucketurls = json.dumps(res, indent=2)
-    s3Minio.putReportFile(
-        GLEANER_MINIO_BUCKET, source_name, "bucketutil_urls.json", bucketurls
-    )
-    get_dagster_logger().info(f"bucker urls report  returned value: {res} ")
+#     graphendpoint = _graphEndpoint()
 
-@op(ins={'source': In(dagster_type=dict)})
-def summarize(context: OpExecutionContext, source: dict):
-    s3Minio = s3.MinioDatastore(
-        _pythonMinioAddress(GLEANER_MINIO_ADDRESS, GLEANER_MINIO_PORT), MINIO_OPTIONS
-    )
-    bucket = GLEANER_MINIO_BUCKET
-    source_name = "SOURCEVAL"
-    endpoint = _graphEndpoint()  # getting data, not uploading data
-    summary_namespace = _graphSummaryEndpoint()
+#     returned_value = generateGraphReportsRepo(
+#         source_name, graphendpoint, reportList=reportTypes["repo_detailed"]
+#     )
+#     r = str("returned value:{}".format(returned_value))
+#     # report = json.dumps(returned_value, indent=2) # value already json.dumps
+#     report = returned_value
+#     s3Minio.putReportFile(bucket, source_name, "graph_stats.json", report)
+#     get_dagster_logger().info(f"graph report  returned  {r} ")
 
-    try:
-        summarydf = get_summary4repoSubset(endpoint, source_name)
-        nt, g = summaryDF2ttl(summarydf, source_name)  # let's try the new generator
-        summaryttl = g.serialize(format="longturtle")
-        # Lets always write out file to s3, and insert as a separate process
-        # we might be able to make this an asset..., but would need to be acessible by http
-        # if not stored in s3
-        objectname = f"{SUMMARY_PATH}/{source_name}_release.ttl"  # needs to match that is expected by post
+# @op(ins={'source': In(dagster_type=dict)})
+# def identifier_stats(context: OpExecutionContext, source: str):
+#     source = getSitemapSourcesFromGleaner(GLEANER_CONFIG_PATH, sourcename="SOURCEVAL")
+#     s3Minio = s3.MinioDatastore(
+#         _pythonMinioAddress(GLEANER_MINIO_ADDRESS, GLEANER_MINIO_PORT), MINIO_OPTIONS
+#     )
+#     bucket = GLEANER_MINIO_BUCKET
+#     source_name = "SOURCEVAL"
 
-        s3Minio.putTextFileToStore(summaryttl, S3ObjectInfo(bucket, objectname))
-        # inserted = sumnsgraph.insert(bytes(summaryttl, 'utf-8'), content_type="application/x-turtle")
-        # if not inserted:
-        #    raise Exception("Loading to graph failed.")
-    except Exception as e:
-        # use dagster logger
-        get_dagster_logger().error(f"Summary. Issue creating graph  {str(e)} ")
-        raise Exception(f"Loading Summary graph failed. {str(e)}")
+#     returned_value = generateIdentifierRepo(source_name, bucket, s3Minio)
+#     r = str("returned value:{}".format(returned_value))
+#     # r = str('identifier stats returned value:{}'.format(returned_value))
+#     report = returned_value.to_json()
+#     s3Minio.putReportFile(bucket, source_name, "identifier_stats.json", report)
+#     get_dagster_logger().info(f"identifer stats report  returned  {r} ")
 
 
-@op(ins={"start": In(Nothing)})
-def upload_summarize(context):
-    returned_value = post_to_graph(
-        "SOURCEVAL",
-        path=SUMMARY_PATH,
-        extension="ttl",
-        graphendpoint=_graphSummaryEndpoint(),
-    )
-    # the above can be done (with a better path approach) in Nabu
-    # returned_value = gleanerio(context, ("object"), "SOURCEVAL")
-    r = str("returned value:{}".format(returned_value))
-    get_dagster_logger().info(f"upload summary returned  {r} ")
+# @op(ins={'source': In(dagster_type=dict)})
+# def bucket_urls(context: OpExecutionContext, source: dict):
+#     s3Minio = s3.MinioDatastore(
+#         f"{GLEANER_MINIO_ADDRESS}:{GLEANER_MINIO_PORT}", MINIO_OPTIONS
+#     )
+#     bucket = GLEANER_MINIO_BUCKET
+#     source_name = "SOURCEVAL"
+
+#     res = s3Minio.listSummonedUrls(bucket, source_name)
+#     bucketurls = json.dumps(res, indent=2)
+#     s3Minio.putReportFile(
+#         GLEANER_MINIO_BUCKET, source_name, "bucketutil_urls.json", bucketurls
+#     )
+#     get_dagster_logger().info(f"bucker urls report  returned value: {res} ")
+
+# @op(ins={'source': In(dagster_type=dict)})
+# def summarize(context: OpExecutionContext, source: dict):
+#     s3Minio = s3.MinioDatastore(
+#         _pythonMinioAddress(GLEANER_MINIO_ADDRESS, GLEANER_MINIO_PORT), MINIO_OPTIONS
+#     )
+#     bucket = GLEANER_MINIO_BUCKET
+#     source_name = "SOURCEVAL"
+#     endpoint = _graphEndpoint()  # getting data, not uploading data
+#     summary_namespace = _graphSummaryEndpoint()
+
+#     try:
+#         summarydf = get_summary4repoSubset(endpoint, source_name)
+#         nt, g = summaryDF2ttl(summarydf, source_name)  # let's try the new generator
+#         summaryttl = g.serialize(format="longturtle")
+#         # Lets always write out file to s3, and insert as a separate process
+#         # we might be able to make this an asset..., but would need to be acessible by http
+#         # if not stored in s3
+#         objectname = f"{SUMMARY_PATH}/{source_name}_release.ttl"  # needs to match that is expected by post
+
+#         s3Minio.putTextFileToStore(summaryttl, S3ObjectInfo(bucket, objectname))
+#         # inserted = sumnsgraph.insert(bytes(summaryttl, 'utf-8'), content_type="application/x-turtle")
+#         # if not inserted:
+#         #    raise Exception("Loading to graph failed.")
+#     except Exception as e:
+#         # use dagster logger
+#         get_dagster_logger().error(f"Summary. Issue creating graph  {str(e)} ")
+#         raise Exception(f"Loading Summary graph failed. {str(e)}")
+
+
+# @op(ins={"start": In(Nothing)})
+# def upload_summarize(context):
+#     returned_value = post_to_graph(
+#         "SOURCEVAL",
+#         path=SUMMARY_PATH,
+#         extension="ttl",
+#         graphendpoint=_graphSummaryEndpoint(),
+#     )
+#     # the above can be done (with a better path approach) in Nabu
+#     # returned_value = gleanerio(context, ("object"), "SOURCEVAL")
+#     r = str("returned value:{}".format(returned_value))
+#     get_dagster_logger().info(f"upload summary returned  {r} ")
 
 @graph
 def harvest(source: str):
 
-    gleaner(getImage())
+    gleaner(getImage(), source)
 
-    # data branch
-    nabu_prune(uploadrelease(naburelease()))
+    # # data branch
+    # nabu_prune(uploadrelease(naburelease()))
 
-    # prov branch
-    nabu_provrelease(nabu_provclear(nabu_provobject(nabu_provdrain())))
+    # # prov branch
+    # nabu_provrelease(nabu_provclear(nabu_provobject(nabu_provdrain())))
 
-    # org branch
-    nabu_orgsrelease(nabuorgs())
+    # # org branch
+    # nabu_orgsrelease(nabuorgs())
 
 
 def generate_job_and_schedules(
