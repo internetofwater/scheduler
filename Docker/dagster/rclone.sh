6fa37bfc8f2d19d01f96a160fe9dbe03930fceb5
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-01-28 12:11:58 -0500

    fix rclone bin

--- Docker/dagster/rclone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# this script downloads and installs rclone
-curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
-unzip rclone-current-linux-amd64.zip
-cd rclone-*-linux-amd64
-cp rclone /usr/bin/
-chown root:root /usr/bin/rclone
-chmod 755 /usr/bin/rclone
-rclone version 
