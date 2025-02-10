2380f7de48a8cc0de9a14531b9b9d8cd593e0ef6
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-02-10 16:47:03 -0500

    Clean up tests and split exports from core assets (#95)
    - create a separate test directory
    - split the dagster code into main.py for the definitions, pipeline.py
    for the core nabu/gleaner ops, and export.py for the external exports
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ userCode/exports.py
@@ -0,0 +1,101 @@
+# Copyright 2025 Lincoln Institute of Land Policy
+# SPDX-License-Identifier: Apache-2.0
+
+from datetime import datetime
+from typing import Optional
+from dagster import OpExecutionContext, asset, get_dagster_logger
+import requests
+from userCode.lib.classes import S3, RcloneClient
+from userCode.lib.dagster import all_dependencies_materialized
+from userCode.lib.env import (
+    GLEANER_GRAPH_URL,
+    GLEANERIO_DATAGRAPH_ENDPOINT,
+    RUNNING_AS_TEST_OR_DEV,
+)
+from userCode.lib.lakefs import LakeFSClient
+from userCode.pipeline import finished_individual_crawl
+
+
+"""
+This file defines all geoconenx exports that move data
+outside of the triplestore. 
+
+"""
+
+
+@asset(deps=[finished_individual_crawl])
+def export_graph_as_nquads(context: OpExecutionContext) -> Optional[str]:
+    """Export the graphdb to nquads"""
+
+    if not all_dependencies_materialized(context, "finished_individual_crawl"):
+        return
+
+    base_url = (
+        GLEANER_GRAPH_URL if not RUNNING_AS_TEST_OR_DEV() else "http://localhost:7200"
+    )
+
+    # Define the repository name and endpoint
+    endpoint = (
+        f"{base_url}/repositories/{GLEANERIO_DATAGRAPH_ENDPOINT}/statements?infer=false"
+    )
+
+    get_dagster_logger().info(
+        f"Exporting graphdb to nquads; fetching data from {endpoint}"
+    )
+    # Download the nq export
+    response = requests.get(
+        endpoint,
+        headers={
+            "Accept": "application/n-quads",
+        },
+    )
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Save the response content to a file
+        with open("outputfile.nq", "wb") as f:
+            f.write(response.content)
+        get_dagster_logger().info("Export of graphdb to nquads successful")
+    else:
+        raise RuntimeError(
+            f"Export failed, status code: {response.status_code} with response {response.text}"
+        )
+
+    s3_client = S3()
+    filename = f"backups/nquads_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}"
+    s3_client.load(response.content, filename)
+    return filename
+
+
+@asset()
+def nquads_to_renci(
+    context: OpExecutionContext,
+    rclone_config: str,
+    export_graph_as_nquads: Optional[str],  # contains the path to the nquads
+):
+    """Upload the nquads to the renci bucket in lakefs"""
+    if (
+        not all_dependencies_materialized(context, "finished_individual_crawl")
+        or not export_graph_as_nquads
+    ):
+        get_dagster_logger().warning(
+            "Skipping rclone copy as all dependencies are not materialized"
+        )
+        return
+
+    if RUNNING_AS_TEST_OR_DEV():
+        get_dagster_logger().warning(
+            "Skipping rclone copy as we are running in test mode"
+        )
+        return
+
+    rclone_client = RcloneClient(rclone_config)
+    lakefs_client = LakeFSClient("geoconnex")
+
+    rclone_client.copy_to_lakefs(
+        destination_branch="develop",
+        destination_filename="iow-dump.nq",
+        path_to_file=export_graph_as_nquads,
+        lakefs_client=lakefs_client,
+    )
+    lakefs_client.merge_branch_into_main(branch="develop")
---
+++ userCode/lib/dagster.py
@@ -0,0 +1,96 @@
+# Copyright 2025 Lincoln Institute of Land Policy
+# SPDX-License-Identifier: Apache-2.0
+
+import json
+from dagster import (
+    AssetKey,
+    DagsterInstance,
+    DynamicPartitionsDefinition,
+    OpExecutionContext,
+    RunFailureSensorContext,
+    get_dagster_logger,
+)
+
+# This is the list of sources to crawl that is dynamically generated at runtime by parsing the geoconnex config
+sources_partitions_def = DynamicPartitionsDefinition(name="sources_partitions_def")
+
+
+def filter_partitions(
+    instance: DagsterInstance, partition_name: str, keys_to_keep: set[str]
+) -> None:
+    """Remove all old partitions that are not in the list of keys to keep.
+    This is needed since dagster does not remove old partitions but keeps them by default for historical monitoring"""
+    sources_partitions_def = instance.get_dynamic_partitions(partition_name)
+    for key in sources_partitions_def:
+        if key not in keys_to_keep:
+            get_dagster_logger().info(
+                f"Deleting partition: {key} from {partition_name}"
+            )
+            instance.delete_dynamic_partition(partition_name, key)
+
+
+def dagster_log_with_parsed_level(structural_log_message: str) -> None:
+    """Try to structurally parse the log message and log it with the appropriate level"""
+    try:
+        json_log = json.loads(structural_log_message)
+    except json.JSONDecodeError:
+        get_dagster_logger().info(structural_log_message)
+        return
+
+    level = json_log["level"]
+    if not level:
+        get_dagster_logger().info(structural_log_message)
+        return
+    msg = json_log["msg"]
+    match level:
+        case "info":
+            get_dagster_logger().info(msg)
+        case "warning" | "warn":
+            get_dagster_logger().warning(msg)
+        case "error":
+            get_dagster_logger().error(msg)
+        case "debug":
+            get_dagster_logger().debug(msg)
+        case "trace":
+            # dagster does not have a trace level
+            get_dagster_logger().debug(msg)
+        case _:
+            get_dagster_logger().info(msg)
+
+    return
+
+
+def all_dependencies_materialized(
+    context: OpExecutionContext, dependency_asset_key: str
+) -> bool:
+    """Check if all partitions of a given asset are materialized"""
+    instance = context.instance
+    all_partitions = sources_partitions_def.get_partition_keys(
+        dynamic_partitions_store=instance
+    )
+    # Check if all partitions of finished_individual_crawl are materialized
+    materialized_partitions = context.instance.get_materialized_partitions(
+        asset_key=AssetKey(dependency_asset_key)
+    )
+
+    if len(all_partitions) != len(materialized_partitions):
+        get_dagster_logger().warning(
+            f"Not all partitions of {dependency_asset_key} are materialized, so nq generation will be skipped"
+        )
+        return False
+    else:
+        get_dagster_logger().info(
+            f"All partitions of {dependency_asset_key} are detected as having been materialized"
+        )
+        return True
+
+
+def slack_error_fn(context: RunFailureSensorContext) -> str:
+    get_dagster_logger().info("Sending notification to Slack")
+    # The make_slack_on_run_failure_sensor automatically sends the job
+    # id and name so you can just send the error. We don't need other data in the string
+    source_being_crawled = context.partition_key
+    if source_being_crawled:
+        return f"Error for partition: {source_being_crawled}: {context.failure_event.message}"
+    else:
+        return f"Error: {context.failure_event.message}"
