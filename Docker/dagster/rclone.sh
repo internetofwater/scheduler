beff5d86782ffd5e4c96b5e3a4c32112d5088448
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-02-13 10:18:51 -0500

    get rid of rclone asset and install it at build time

---
+++ Docker/dagster/rclone.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+
+# Detect platform
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+# Map OS to rclone's naming convention
+case "$OS" in
+    Linux) OS="linux" ;;
+    Darwin) OS="osx" ;;
+    FreeBSD) OS="freebsd" ;;
+    *) echo "Unsupported OS: $OS"; exit 1 ;;
+esac
+
+# Map architecture to rclone's naming convention
+case "$ARCH" in
+    x86_64) ARCH="amd64" ;;
+    armv7l) ARCH="arm" ;;
+    aarch64) ARCH="arm64" ;;
+    arm64) ARCH="arm64" ;;
+    i386) ARCH="386" ;;
+    *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+esac
+
+# Construct download URL
+FILENAME="rclone-current-${OS}-${ARCH}.zip"
+URL="https://downloads.rclone.org/${FILENAME}"
+
+echo "Downloading rclone for ${OS}-${ARCH}..."
+curl -O "$URL"
+
+# Unzip and install
+unzip "$FILENAME"
+cd rclone-*-${OS}-${ARCH}
+sudo cp rclone /usr/bin/
+sudo chown root:root /usr/bin/rclone
+sudo chmod 755 /usr/bin/rclone
+
+echo "Installation complete!"
