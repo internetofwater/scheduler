bcdc7fdd0c55d71d7fd894317bd61c42cebdd28c
Benjamin Webb <benjamin.miller.webb@gmail.com>
2025-02-19 14:26:02 -0500

    Delete dagster postgres

--- main.py
+++ main.py
@@ -82,7 +82,6 @@ def up(profiles: list[str], build: bool = False, detach: bool = False):
     command = f"docker compose {profileCommand} -f Docker/Docker-compose.yaml up"
     if "production" not in profiles:
         command = "DAGSTER_POSTGRES_HOST=0.0.0.0 " + command
-
     if build:
         command += " --build"
     if detach:
@@ -166,7 +165,7 @@ def main():
 
     if args.command == "down":
         run_subprocess(
-            "DAGSTER_POSTGRES_HOST=dagster_postgres docker compose --profile localInfra --profile production -f Docker/Docker-compose.yaml down"
+            "docker compose --profile localInfra --profile production -f Docker/Docker-compose.yaml down"
         )
     elif args.command == "dagster-dev":
         run_subprocess("DAGSTER_POSTGRES_HOST=0.0.0.0 dagster dev")
