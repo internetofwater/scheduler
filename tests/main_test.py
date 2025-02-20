84d97bb99f08ebe5df8ea6e26e349498daedcd17
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-02-20 12:34:11 -0500

    add export test

--- tests/main_test.py
+++ tests/main_test.py
@@ -15,7 +15,7 @@ from userCode.pipeline import sources_partitions_def
 
 from dagster import AssetsDefinition, AssetSpec, SourceAsset
 
-from .lib.helpers import execute_sparql, clear_graph
+from .helpers import execute_sparql, clear_graph
 
 
 def assert_data_is_linked_in_graph():
@@ -38,9 +38,9 @@ def assert_data_is_linked_in_graph():
     """
     resultDict = execute_sparql(query)
     # make sure that the florida canal monitoring location is on the florida river mainstem
-    assert (
-        len(resultDict["monitoringLocation"]) > 0
-    ), "There were no linked monitoring locations for the Florida River Mainstem"
+    assert len(resultDict["monitoringLocation"]) > 0, (
+        "There were no linked monitoring locations for the Florida River Mainstem"
+    )
     assert (
         "https://geoconnex.us/cdss/gages/FLOCANCO" in resultDict["monitoringLocation"]
     )
@@ -93,9 +93,9 @@ def test_e2e():
     """
 
     resultDict = execute_sparql(query)
-    assert (
-        "Florida River" in resultDict["o"]
-    ), "The Florida River Mainstem was not found in the graph"
+    assert "Florida River" in resultDict["o"], (
+        "The Florida River Mainstem was not found in the graph"
+    )
 
     result = resolved_job.execute_in_process(
         instance=instance, partition_key="cdss_co_gages__0"
@@ -105,6 +105,12 @@ def test_e2e():
     assert_data_is_linked_in_graph()
     # Don't want to actually transfer the file but should check it is installed
     assert_rclone_is_installed_properly()
+
+    assert (
+        definitions.get_job_def("export_nquads")
+        .execute_in_process(instance=instance)
+        .success
+    )
 
 
 def test_dynamic_partitions():
